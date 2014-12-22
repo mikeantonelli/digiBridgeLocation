aa76c519ad70e82e6bf62e1db3889aadb63d082a
Mike Antonelli <mike.antonelli@me.com>
2014-12-22 10:00:21 -0600

    Initial Commit

---
+++ digiBridgeLocation/digiBridgeLocation.h
@@ -0,0 +1,13 @@
+//
+//  digiBridgeLocation.h
+//  digiBridgeLocation
+//
+//  Created by Mike Antonelli on 12/22/14.
+//  Copyright (c) 2014 Mike Antonelli. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface digiBridgeLocation : NSObject
+
+@end
---
+++ digiBridgeLocation/digiBridgeLocation.m
@@ -0,0 +1,13 @@
+//
+//  digiBridgeLocation.m
+//  digiBridgeLocation
+//
+//  Created by Mike Antonelli on 12/22/14.
+//  Copyright (c) 2014 Mike Antonelli. All rights reserved.
+//
+
+#import "digiBridgeLocation.h"
+
+@implementation digiBridgeLocation
+
+@end
